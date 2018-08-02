346fd694be747b5ecc1f915cd53742162568872b
Bill <ceriustech@gmail.com>
2018-08-01 23:59:53 -0500

    Changes made to multiple files.

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,23 @@
-$(function() {
-  
-  $(".menu-link").click(function(e) {
-    e.preventDefault();
-    
-    $(".menu-overlay").toggleClass("open");
-    $(".menu").toggleClass("open");
+ //<select view>
+//<init slideshow container>
+var slideIndex = 0;
+showSlides();
 
-  });
-	
-});
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var faCircle = document.getElementsByClassName("fa-circle");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < faCircle.length; i++) {
+        faCircle[i].className = faCircle[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    faCircle[slideIndex-1].className += " active";
+    setTimeout(showSlides, 7000); // Change image every 4 seconds
+}
+//</init>
+//<select view>
