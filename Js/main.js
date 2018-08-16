e7b3e85b50870540d78f91c838ba824227cd5aae
Bill <ceriustech@gmail.com>
2018-08-15 22:38:52 -0500

    Changes made to multiple files.

--- Js/main.js
+++ Js/main.js
@@ -1,23 +1,30 @@
  //<select view>
 //<init slideshow container>
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var faCircle = document.getElementsByClassName("fa-circle");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < faCircle.length; i++) {
-        faCircle[i].className = faCircle[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    faCircle[slideIndex-1].className += " active";
-    setTimeout(showSlides, 7000); // Change image every 4 seconds
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var faCircle = document.getElementsByClassName("fa-circle");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < faCircle.length; i++) {
+      faCircle[i].className = faCircle[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  faCircle[slideIndex-1].className += " active";
 }
 //</init>
 //<select view>
