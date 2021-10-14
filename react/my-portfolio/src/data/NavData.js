b7cb011c9e3a47d959453914011b0d6a2eb77281
Bill <ceriustech@gmail.com>
2021-10-13 22:52:36 -0500

    added dependencies, folder structure, and boilerplate data

---
+++ react/my-portfolio/src/data/NavData.js
@@ -0,0 +1,99 @@
+// import HomePage from './pages/HomePage';
+// import AboutPage from './pages/AboutPage';
+// import Dashboard from './pages/Dashboard';
+// import AccountPage from './pages/AccountPage';
+
+export const navItems = [
+	{
+		linkName: 'Home',
+		component: HomePage,
+		path: '/',
+		subNav: [
+			{
+				subLinkName: 'Sub Link 1',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 2',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 3',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 4',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+		],
+	},
+	{
+		linkName: 'About',
+		component: AboutPage,
+		path: '/about',
+		subNav: [
+			{
+				subLinkName: 'Sub Link 1',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 2',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 3',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 4',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+		],
+	},
+	{
+		linkName: 'Dashboard',
+		component: Dashboard,
+		path: '/dashboard',
+		subNav: [
+			{
+				subLinkName: 'Sub Link 1',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 2',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 3',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 4',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+		],
+	},
+	{
+		linkName: 'Account',
+		component: AccountPage,
+		path: '/account',
+		subNav: [
+			{
+				subLinkName: 'Sub Link 1',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 2',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 3',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+			{
+				subLinkName: 'Sub Link 4',
+				path: 'https://www.youtube.com/watch?v=7UMuJMiNjSk',
+			},
+		],
+	},
+];
